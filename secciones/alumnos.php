77791171107c209a67a6d308e70ae4c3e5b7119f
Leonardo Díaz <107229055+LeonardoDiaz1@users.noreply.github.com>
2024-04-29 19:33:10 -0600

    Update alumnos.php

--- secciones/alumnos.php
+++ secciones/alumnos.php
@@ -1,8 +1,10 @@
 <?php
-
+// Se incluye el archivo de configuración de la base de datos
 include_once '../configuraciones/bd.php';
+// Se crea una instancia de la conexión a la base de datos
 $conexionBD=BD::crearInstancia();
 
+// Se obtienen los datos del formulario o se asignan valores predeterminados
 $id=isset($_POST['id'])?$_POST['id']:'';
 $nombre=isset($_POST['nombre'])?$_POST['nombre']:'';
 $apellidos=isset($_POST['apellidos'])?$_POST['apellidos']:'';
@@ -10,18 +12,21 @@ $apellidos=isset($_POST['apellidos'])?$_POST['apellidos']:'';
 $cursos=isset($_POST['cursos'])?$_POST['cursos']:'';
 $accion=isset($_POST['accion'])?$_POST['accion']:'';
 
-
+// Si se ha enviado un formulario, se ejecuta la acción correspondiente
 if($accion!=''){
     switch($accion){
         case 'agregar':
+            // Se inserta un nuevo alumno en la base de datos
             $sql="INSERT INTO alumnos (id, nombre, apellidos) VALUES (NULL, :nombre, :apellidos)";
             $consulta=$conexionBD->prepare($sql);
             $consulta->bindParam(':nombre',$nombre);
             $consulta->bindParam(':apellidos',$apellidos);
             $consulta->execute();
-
+        
+            // Se obtiene el ID del nuevo alumno insertado
             $idAlumno=$conexionBD->lastInsertId();
 
+            // Se insertan los cursos asociados al alumno en la tabla de relación alumnos_cursos
             foreach($cursos as $curso){
                 $sql="INSERT INTO alumnos_cursos (id, idalumno, idcurso) VALUES (NULL, :idalumno, :idcurso)";
                 $consulta=$conexionBD->prepare($sql);
@@ -33,7 +38,7 @@ if($accion!=''){
         break;
 
         case 'Seleccionar':
-            
+            // Selecciona un alumno específico y sus cursos asociados para su edición
             $sql="SELECT * FROM alumnos WHERE id=:id";
             $consulta=$conexionBD->prepare($sql);
             $consulta->bindParam(':id',$id);
